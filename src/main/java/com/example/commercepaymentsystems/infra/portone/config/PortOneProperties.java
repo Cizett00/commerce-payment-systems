865cb2c6cc11220e862e5ec3e5f42cec2fb2207d
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-08-21 14:57:17 +0900

    add(payment-portone): PortOneProperties 및 PortOneConfig 추가
    - PortOneProperties로 application.properties의 정보 가져와 Bean에 등록
    - PortOneConfig로 PortOne과 통신할 RestClient 객체 생성 및 등록

---
+++ src/main/java/com/example/commercepaymentsystems/infra/portone/config/PortOneProperties.java
@@ -0,0 +1,16 @@
+package com.example.commercepaymentsystems.infra.portone.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter @Setter
+@ConfigurationProperties(prefix = "portone")
+public class PortOneProperties {
+    private String baseUrl;
+    private String apiSecret;
+    private String storeId;
+    private String channelKey;
+}
