e90cfc123bfa1ffe553177652e35facf6139e94f
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-08-24 00:59:18 +0900

    add(payment-webhook): WebhookEventService 구현
    - 상태 변경 메서드 구현
    - saveIfNotDuplicate로 저장된 데이터 없으면 저장하는 메서드 구현

--- src/main/java/com/example/commercepaymentsystems/common/exception/ErrorCode.java
+++ src/main/java/com/example/commercepaymentsystems/common/exception/ErrorCode.java
@@ -46,7 +46,10 @@ public enum ErrorCode {
     //cart (CART_xxx)
     CART_EMPTY(HttpStatus.BAD_REQUEST, "CART_001", "장바구니가 비어있습니다." ),
     INVALID_QUANTITY(HttpStatus.BAD_REQUEST, "CART_003", "수량은 1 이상이어야 합니다."),
-    STOCK_EXCEEDED(HttpStatus.CONFLICT,"CART_004", "재고가 부족하여 담을 수 없습니다.");
+    STOCK_EXCEEDED(HttpStatus.CONFLICT,"CART_004", "재고가 부족하여 담을 수 없습니다."),
+
+    //webhook (WEBHOOK_xxx)
+    WEBHOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "WEBHOOK_001", "웹훅을 찾을 수 없습니다.");
 
 
     private final HttpStatus status;
