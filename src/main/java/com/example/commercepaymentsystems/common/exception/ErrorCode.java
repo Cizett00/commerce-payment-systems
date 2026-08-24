ae09e2f3caf4788d0af1f14052a538ff0bb719b9
ihateahri <ihateahri@naver.com>
2026-08-24 15:47:32 +0900

    feat: OrderService 포인트 부족 에러코드 수정 / 에러코드 추가

--- src/main/java/com/example/commercepaymentsystems/common/exception/ErrorCode.java
+++ src/main/java/com/example/commercepaymentsystems/common/exception/ErrorCode.java
@@ -36,6 +36,7 @@ public enum ErrorCode {
     ORDER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ORDER_006", "본인의 주문만 조회할 수 있습니다."),
     ORDER_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "ORDER_007", "인증이 필요합니다."),
     INVALID_ORDER_STATUS(HttpStatus.CONFLICT, "ORDER_008", "변경할 수 없는 주문 상태입니다."),
+    POINT_EXCEEDS_ORDER_AMOUNT(HttpStatus.BAD_REQUEST, "ORDER_009", "사용 포인트는 주문 금액을 초과할 수 없습니다."),
   
     //payments (PAYMENT_xxx)
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "PAYMENT_001", "결제 정보를 찾을 수 없습니다."),
