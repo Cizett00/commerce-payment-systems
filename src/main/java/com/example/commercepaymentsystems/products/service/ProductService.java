28efeab9d1d0c122954069ee66a596eab819497e
조성연 <hiskybj000@gmail.com>
2026-08-24 14:04:45 +0900

    필터 구현

--- src/main/java/com/example/commercepaymentsystems/products/service/ProductService.java
+++ src/main/java/com/example/commercepaymentsystems/products/service/ProductService.java
@@ -15,11 +15,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class ProductService {
     public final ProductRepository productRepository;
 
