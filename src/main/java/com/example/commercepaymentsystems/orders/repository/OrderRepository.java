eb733db7ce20925ecae2c6e99e341ac2f2dc3e4d
ihateahri <ihateahri@naver.com>
2026-08-24 14:07:58 +0900

    feat: Order 엔티티 pointUsed 필드 추가

--- src/main/java/com/example/commercepaymentsystems/orders/repository/OrderRepository.java
+++ src/main/java/com/example/commercepaymentsystems/orders/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.commercepaymentsystems.orders.repository;
 
 import com.example.commercepaymentsystems.orders.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // 생성일 기준 최신 주문부터 조회
-    List<Order> findByCustomer_IdOrderByCreatedAtDesc(Long customerId);
+    Page<Order> findByCustomer_IdOrderByCreatedAtDesc(Long customerId, Pageable pageable);
 
     // 주문 상세 조회
     Optional<Order> findByIdAndCustomer_Id(Long orderId, Long customerId);
