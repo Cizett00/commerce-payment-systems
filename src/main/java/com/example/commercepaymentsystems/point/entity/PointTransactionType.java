467dd5b899597ec7a148722424d094c1cbc52b61
GaryKang0208 <khb1661@gmail.com>
2026-08-24 12:22:47 +0900

    포인트 변동사항 ENUM 추가

---
+++ src/main/java/com/example/commercepaymentsystems/point/entity/PointTransactionType.java
@@ -0,0 +1,10 @@
+package com.example.commercepaymentsystems.point.entity;
+
+import com.example.commercepaymentsystems.payments.entity.PaymentStatus;
+
+public enum PointTransactionType {
+    EARN,
+    USE,
+    REVOKEEARN,
+    RESTOREUSE
+}
